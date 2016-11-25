1ac645df1170b7a9820caa36f36db6380eefdee3
BrainRTP <brainrtp@yandex.ru>
2016-11-26 00:28:51 +0600

    Обновлена Constants.java
    - Частично переведено на русский язык
    - Добавлены цветовые коды

--- src/me/igorek536/JRPlayer/Globals/Constants.java
+++ src/me/igorek536/JRPlayer/Globals/Constants.java
@@ -5,24 +5,24 @@ public class Constants {
 
 
     public static final String PROG_FILELIST_NAME = "list.json";
-    public static final String PROG_FILELIST_EXISTS = "File already exists!";
+    public static final String PROG_FILELIST_EXISTS = "\033[32mФайл радиостанций доступен. Программа запущена. Приятного пользования!\033[0m";
     public static final String PROG_FILELIST_CREATED = "File succesfull created!";
     public static final String PROG_FILELIST_ERROR = "Error while creating default file!";
 
     public static final String PROG_RADIO_SELECT = "Please, select radiostarton! Enter id!";
     public static final String PROG_RADIO_SELECTED = "Radio stream successfull selectde! Yeah!";
 
-    public static final String PROG_LAUNCHED = "Program launched at ";
-    public static final String PROG_TYPE_HELP = "Type 'help' for help!";
+    public static final String PROG_LAUNCHED = "\033[32mЗапуск програмы от \033[0m";
+    public static final String PROG_TYPE_HELP = "\033[32mПропиши '\033[31mhelp\033[32m', чтобы узнать информацию!\033[0m";
 
-    public static final String PROG_PLAYER_PLAY = "Music is playing! Selected station is: ";
-    public static final String PROG_PLAYER_STOP = "Music stoped!";
-    public static final String PROG_PLAYER_PLAYING = "Music already playing!";
-    public static final String PROG_PLAYER_STOPING = "Nothing to stop!";
+    public static final String PROG_PLAYER_PLAY = "\033[32mРадио работает. Играет радиостанция: \033[39m";
+    public static final String PROG_PLAYER_STOP = "\033[32mМузыка выключена!\033[0m";
+    public static final String PROG_PLAYER_PLAYING = "\033[31mМузыка уже играет!\033[0m";
+    public static final String PROG_PLAYER_STOPING = "\033[31mНечего выключать xD!\033[0m";
 
-    public static final String PROG_ERROR_PLAYER = "Error!";
+    public static final String PROG_ERROR_PLAYER = "\033[31mОшибка!\033[0m";
     public static final String PROG_ERROR_RADIO = "No radio selected error!";
-    public static final String PROG_ERROR_COMAND = "Wrong command!";
+    public static final String PROG_ERROR_COMAND = "\033[31mКоманда не найдена!\033[0m";
     public static final String PROG_ERROR_JSON = "Error while parsing json!";
     public static final String PROG_ERROR_NOARG = "You enter no argument!";
     public static final String PROG_ERROR_SELECT = "Something was wrong!";
